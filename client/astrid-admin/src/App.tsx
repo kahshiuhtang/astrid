51b1b759c63f72f91fa4e3d25cb5be9c23dea885
Kah Shiuh Tang <kahshiuh.tang@gmail.com>
2024-08-23 18:17:38 -0400

    start layout of modules

--- client/astrid-admin/src/App.tsx
+++ client/astrid-admin/src/App.tsx
@@ -1,34 +1,47 @@
-import { Table } from "@radix-ui/themes";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "@/home/homePage";
+import RootBoundary from "@/errors/rootBoundary";
+import AccountingPage from "@/modules/accounting/accountingPage";
+import CustomersPage from "@/modules/customers/customersPage";
+import FilesPage from "@/modules/files/filesPage";
+import WebDesignPage from "@/modules/webDesign/webDesignPage";
+import TicketsPage from "@/modules/tickets/ticketsPage";
+import InventoryPage from "@/modules/inventory/inventoryPage";
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <HomePage />,
+        errorElement: <RootBoundary />,
+    },
+    {
+        path: "accounting",
+        element: <AccountingPage />,
+    },
+    {
+        path: "customers",
+        element: <CustomersPage />,
+    },
+    {
+        path: "accounting",
+        element: <AccountingPage />,
+    },
+    {
+        path: "files",
+        element: <FilesPage />,
+    },
+    {
+        path: "inventory",
+        element: <InventoryPage />,
+    },
+    {
+        path: "tickets",
+        element: <TicketsPage />,
+    },
+    {
+        path: "web",
+        element: <WebDesignPage />,
+    },
+]);
 export default function App() {
-    return (
-        <Table.Root>
-            <Table.Header>
-                <Table.Row>
-                    <Table.ColumnHeaderCell>Full name</Table.ColumnHeaderCell>
-                    <Table.ColumnHeaderCell>Email</Table.ColumnHeaderCell>
-                    <Table.ColumnHeaderCell>Group</Table.ColumnHeaderCell>
-                </Table.Row>
-            </Table.Header>
-
-            <Table.Body>
-                <Table.Row>
-                    <Table.RowHeaderCell>Danilo Sousa</Table.RowHeaderCell>
-                    <Table.Cell>danilo@example.com</Table.Cell>
-                    <Table.Cell>Developer</Table.Cell>
-                </Table.Row>
-
-                <Table.Row>
-                    <Table.RowHeaderCell>Zahra Ambessa</Table.RowHeaderCell>
-                    <Table.Cell>zahra@example.com</Table.Cell>
-                    <Table.Cell>Admin</Table.Cell>
-                </Table.Row>
-
-                <Table.Row>
-                    <Table.RowHeaderCell>Jasper Eriksson</Table.RowHeaderCell>
-                    <Table.Cell>jasper@example.com</Table.Cell>
-                    <Table.Cell>Developer</Table.Cell>
-                </Table.Row>
-            </Table.Body>
-        </Table.Root>
-    );
+    return <RouterProvider router={router} />;
 }

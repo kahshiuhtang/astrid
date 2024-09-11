79639e0dbe3a751b07bbce8c424e8de2b0d93c83
Kah Shiuh Tang <kahshiuh.tang@gmail.com>
2024-09-11 17:11:07 -0400

    react router package

--- astrid-fe/src/main.tsx
+++ astrid-fe/src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import RootLayout from "@/routes/layout/root-layout";
+import HomePage from "@/features/landing/HomePage";
+const router = createBrowserRouter([
+    {
+        element: <RootLayout />,
+        children: [
+            { path: "/", element: <HomePage /> },
+        ],
+    },
+]);
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);

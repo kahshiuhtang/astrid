7ef2cb2e0ce5214b8a7c1aa9e4db1f57924a7e00
Kah Shiuh Tang <kahshiuh.tang@gmail.com>
2024-09-16 23:36:37 -0400

    switch to nextjs for SSR

--- astrid-fe/src/features/landing/HomePage.tsx
+++
@@ -1,9 +0,0 @@
-import HomeNavbar from "@/components/navbar/home-navbar";
-
-export default function HomePage() {
-    return (
-        <div>
-            <HomeNavbar />
-        </div>
-    );
-}
--- astrid-fe/src/features/landing/LandingPage.tsx
+++
@@ -1,9 +0,0 @@
-import LandingNavbar from "@/components/navbar/landing-navbar";
-
-export default function LandingPage() {
-    return (
-        <div>
-            <LandingNavbar />
-        </div>
-    );
-}

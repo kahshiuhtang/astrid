5db7f030529c4f690e7a1d3e5b9d2816f7b64d30
Kah Shiuh Tang <kahshiuh.tang@gmail.com>
2024-09-20 12:57:18 -0400

    starting of form for submitting data source information

---
+++ astrid-fe/src/app/data/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import CreateDataSourceCardAST from "@/app/data/createDataSource";
+
+export default function DataPageAST() {
+    return (
+        <div>
+            <CreateDataSourceCardAST />
+        </div>
+    );
+}

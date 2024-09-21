3a5891ba0621ed624d9358c187997c542b53d29f
Kah Shiuh Tang <kahshiuh.tang@gmail.com>
2024-09-21 17:47:48 -0400

    add service searching, and two more components

--- astrid-fe/src/app/services/page.tsx
+++ astrid-fe/src/app/services/page.tsx
@@ -1,10 +1,14 @@
 "use client";
 
+import ServicesSearchBarAST from "./searchBar";
 import ServicesTableAST from "./servicesTable";
 
 export default function ServicesPageAST() {
     return (
         <div>
+            <div className="w-full flex justify-center">
+                <ServicesSearchBarAST />
+            </div>
             <div className="p-2">
                 <ServicesTableAST />
             </div>
